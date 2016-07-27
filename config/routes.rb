90539e07af409411e0a7d2da6c5d65a0a227281c
Adam Lesniak <adam@lesniak.pl>
2016-07-27 20:39:14 +0000

    Add log in and log out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
 
   get 'sign_up', to: 'users#new'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   resources :users, :except => :new
   resources :articles, :except => :index
