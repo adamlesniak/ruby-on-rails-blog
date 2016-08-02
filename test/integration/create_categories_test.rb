ac5ef7d32aaed978bcda9eb327c7b675fddf5428
Adam Lesniak <adam@lesniak.pl>
2016-08-02 22:53:38 +0000

    Fix tests and update navbar with new menu item - Categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,7 +2,14 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
   
+  
+  def setup
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+  end
+  
+  
   test "get new category form and create category" do
+    sign_in_as(@user, 'password')
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
@@ -12,7 +19,9 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_match "sports", response.body
   end
   
+  
   test "invalid category submission results in failure" do
+    sign_in_as(@user, 'password')
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count', 1 do
@@ -21,7 +30,6 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_template 'categories/new'
     assert_select 'h2.panel-title'
     assert_select 'ul.errors'
-    
   end
 
 end
